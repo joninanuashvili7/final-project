cd243be67d44d03415b24be1c549d214a542b3e8
joninanuashvili7 <joninanuashvili05@gmail.com>
2024-03-07 18:43:36 +0400

    final project

---
+++ script.js
@@ -0,0 +1,18 @@
+let toggleButton = document.getElementById('toggleburger');
+let navigation = document.getElementById('nav');
+let header = document.getElementById('header');
+
+toggleButton.addEventListener('click', function(){
+    navigation.classList.toggle('active');
+})
+
+toggleButton.addEventListener('click', function(){
+    header.classList.toggle('head');
+})
+
+
+
+document.getElementById('buttonclick').addEventListener('click', function()
+{
+    alert('Thank you for getting in touch!')
+})
